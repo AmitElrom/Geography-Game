1debf30be5409a8b8e756a28fad5d7868b1c00bb
Amit Elrom <amitelrom99@gmail.com>
2022-08-17 13:13:39 +0300

    build sign up validation

--- src/components/Authentication/SignUp/SignUp.js
+++ src/components/Authentication/SignUp/SignUp.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import { useRef } from "react";
+
+import {
+  isEmail,
+  isNotEmpty,
+  isValidPassword,
+} from "../../../utils/utils-validity";
 
 const SignUp = () => {
-    return (
-        <div>SignUp</div>
-    )
-}
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const password1Ref = useRef();
+  const password2Ref = useRef();
 
-export default SignUp
+  const submitSignUpFormHandler = (e) => {
+    e.preventDefault();
+
+    let enteredfirstName = firstNameRef.current.value;
+    let enteredLastName = lastNameRef.current.value;
+    let enteredEmail = emailRef.current.value;
+    let enteredPassword1 = password1Ref.current.value;
+    let enteredPassword2 = password2Ref.current.value;
+
+    let firstNameIsValid = isNotEmpty(enteredfirstName);
+    let lastNameIsValid = isNotEmpty(enteredLastName);
+    let emailIsValid = isEmail(enteredEmail);
+    let password1IsValid = isValidPassword(enteredPassword1);
+    let password2IsValid =
+      isValidPassword(enteredPassword2) &&
+      enteredPassword1 === enteredPassword2;
+
+    console.log(
+      enteredfirstName,
+      enteredLastName,
+      enteredEmail,
+      enteredPassword1,
+      enteredPassword2
+    );
+
+    console.log(
+      firstNameIsValid,
+      lastNameIsValid,
+      emailIsValid,
+      password1IsValid,
+      password2IsValid
+    );
+  };
+
+  return (
+    <form onSubmit={submitSignUpFormHandler}>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" id="first_name" ref={firstNameRef} />
+      </div>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" id="last_name" ref={lastNameRef} />
+      </div>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" id="email" ref={emailRef} />
+      </div>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" id="password1" ref={password1Ref} />
+      </div>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" id="password2" ref={password2Ref} />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUp;
---
+++ src/utils/utils-validity.js
@@ -0,0 +1,31 @@
+const hasWhiteSpace = value => {
+    return /\s/g.test(value);
+}
+
+const isEmail = value => {
+    if (value.includes('@')) {
+        const valueArr = [];
+        for (let i = 0; i < value.length; i++) {
+            valueArr.push(value[i]);
+        }
+        let valueAtIndex = +valueArr.findIndex(char => char === '@');
+        console.log(valueAtIndex);
+        if (valueArr[valueAtIndex - 1] && valueArr[valueAtIndex + 1]) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+};
+
+const isNotEmpty = value => {
+    return value.trim().length !== 0;
+};
+
+const isValidPassword = value => {
+    return !hasWhiteSpace(value) && value.length >= 8 && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value)
+};
+
+export { isEmail, isNotEmpty, isValidPassword };
