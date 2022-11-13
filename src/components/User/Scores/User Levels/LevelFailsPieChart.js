b966b21b7415a0d6efaf90f751e47d201d072356
Amit Elrom <amitelrom99@gmail.com>
2022-11-14 00:53:18 +0200

    on pie chart

--- src/components/User/Scores/User Levels/LevelFailsPieChart.js
+++ src/components/User/Scores/User Levels/LevelFailsPieChart.js
@@ -1,31 +1,105 @@
 import React from "react";
 import { Chart } from "react-google-charts";
+import { ResponsiveContainer, PieChart, Pie, Tooltip, Cell } from "recharts";
 
-const LevelFailsPieChart = ({ fails, title }) => {
-  const options = {
-    legend: "none",
-    pieSliceText: "label",
-    title: `Countries you failed to answer right on them, at ${title} level`,
-    pieStartAngle: 100,
-  };
+const COLORS = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+];
 
-  const transformedFails = fails.map((fail) => {
-    return [fail.countryName, fail.falseCountries.length];
-  });
-
-  const data = [
-    ["Country", "Number of times mistaking this country."],
-    ...transformedFails,
-  ];
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = (props) => {
+  console.log("props", props);
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+    countryName,
+  } = props;
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"400px"}
-    />
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {/* {`${(percent * 100).toFixed(0)}%`} */}
+      {countryName}
+    </text>
+  );
+};
+
+const CustomTooltip = (props) => {
+  console.log("props tooltip", props);
+  return (
+    <div>
+      <p className="desc">Anything you want can be displayed here.</p>
+    </div>
+  );
+};
+
+const LevelFailsPieChart = ({ fails, title }) => {
+  const transformedFails = fails.map((fail) => {
+    return {
+      countryName: fail.countryName,
+      numFalseCountries: fail.falseCountries.length,
+    };
+  });
+
+  return (
+    <PieChart width={250} height={250}>
+      <Pie
+        isAnimationActive
+        data={transformedFails}
+        dataKey="numFalseCountries"
+        nameKey="countryName"
+        cx="50%"
+        cy="50%"
+        outerRadius={50}
+        fill="#8884d8"
+        label={renderCustomizedLabel}
+        labelLine={false}
+      >
+        {transformedFails.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip
+        contentStyle={{ backgroundColor: "red" }}
+        content={<CustomTooltip />}
+      />
+    </PieChart>
   );
 };
 
