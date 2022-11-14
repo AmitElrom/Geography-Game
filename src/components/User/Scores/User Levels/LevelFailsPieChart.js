406466ae3f3888c98132bda406def95d5a34aa69
Amit Elrom <amitelrom99@gmail.com>
2022-11-14 11:56:45 +0200

    on pie

--- src/components/User/Scores/User Levels/LevelFailsPieChart.js
+++ src/components/User/Scores/User Levels/LevelFailsPieChart.js
@@ -1,73 +1,5 @@
 import React from "react";
-import { Chart } from "react-google-charts";
-import { ResponsiveContainer, PieChart, Pie, Tooltip, Cell } from "recharts";
-
-const COLORS = [
-  "#0088FE",
-  "#00C49F",
-  "#FFBB28",
-  "#FF8042",
-  "#0088FE",
-  "#00C49F",
-  "#FFBB28",
-  "#FF8042",
-  "#0088FE",
-  "#00C49F",
-  "#FFBB28",
-  "#FF8042",
-  "#0088FE",
-  "#00C49F",
-  "#FFBB28",
-  "#FF8042",
-  "#0088FE",
-  "#00C49F",
-  "#FFBB28",
-  "#FF8042",
-  "#0088FE",
-  "#00C49F",
-  "#FFBB28",
-  "#FF8042",
-];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = (props) => {
-  console.log("props", props);
-  const {
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent,
-    index,
-    countryName,
-  } = props;
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {/* {`${(percent * 100).toFixed(0)}%`} */}
-      {countryName}
-    </text>
-  );
-};
-
-const CustomTooltip = (props) => {
-  console.log("props tooltip", props);
-  return (
-    <div>
-      <p className="desc">Anything you want can be displayed here.</p>
-    </div>
-  );
-};
+import { PieChart, Pie, Tooltip } from "recharts";
 
 const LevelFailsPieChart = ({ fails, title }) => {
   const transformedFails = fails.map((fail) => {
@@ -80,25 +12,17 @@ const LevelFailsPieChart = ({ fails, title }) => {
   return (
     <PieChart width={250} height={250}>
       <Pie
-        isAnimationActive
-        data={transformedFails}
         dataKey="numFalseCountries"
         nameKey="countryName"
+        isAnimationActive={false}
+        data={transformedFails}
         cx="50%"
         cy="50%"
-        outerRadius={50}
+        outerRadius={80}
         fill="#8884d8"
-        label={renderCustomizedLabel}
-        labelLine={false}
-      >
-        {transformedFails.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip
-        contentStyle={{ backgroundColor: "red" }}
-        content={<CustomTooltip />}
+        label
       />
+      <Tooltip />
     </PieChart>
   );
 };
