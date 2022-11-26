28a8546d2176747b0efa9d558eab85f92f807e3c
Amit Elrom <amitelrom99@gmail.com>
2022-11-26 21:45:27 +0200

    fix: position tooltip pie chart

--- src/components/User/Scores/User Levels/LevelFailsPieChart.js
+++ src/components/User/Scores/User Levels/LevelFailsPieChart.js
@@ -1,28 +1,49 @@
-import React from "react";
-import { PieChart, Pie, Tooltip, Label } from "recharts";
+import React, { Fragment, useState } from "react";
+import { PieChart, Pie, Tooltip, Cell } from "recharts";
 import CustomToolTip from "./CustomToolTip";
 
-const LevelFailsPieChart = ({ fails, title }) => {
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
+const LevelFailsPieChart = ({ fails, title }) => {
   const renderLabel = (entry) => {
-    return entry.name;
-  }
+    console.log(entry);
+    return (entry.percent * 100) % 10 === 0
+      ? `${(entry.percent * 100).toFixed(0)}%`
+      : `${(entry.percent * 100).toFixed(2)}%`;
+  };
 
   return (
-    <PieChart width={400} height={350}>
-      <Pie
-        dataKey="numOfFails"
-        nameKey="countryName"
-        isAnimationActive={false}
-        data={fails}
-        cx="50%"
-        cy="50%"
-        outerRadius={50}
-        fill="#8884d8"
-        label={renderLabel}
-      />
-      <Tooltip content={<CustomToolTip />} />
-    </PieChart>
+    <Fragment>
+      <h5>{title} failures - the country flags you got wrong</h5>
+      <PieChart width={250} height={250}>
+        <Pie
+          overflow="hidden"
+          dataKey="numOfFails"
+          nameKey="countryName"
+          isAnimationActive={false}
+          data={fails}
+          cx="50%"
+          cy="50%"
+          outerRadius={50}
+          fill="#8884d8"
+          label={renderLabel}
+        >
+          {fails.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip
+          wrapperStyle={{
+            width: "max-content",
+            zIndex: 10,
+            position: "fixed",
+            top: "20%",
+            left: "25%",
+          }}
+          content={<CustomToolTip />}
+        />
+      </PieChart>
+    </Fragment>
   );
 };
 
